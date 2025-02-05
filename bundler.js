390439c90c72b67890e1838e7688fb81917cdc5a
Jean-Denis Boivin <jd@boiv.in>
2025-02-05 11:45:54 -0500

    Remove quotes

--- bundler.js
+++ bundler.js
@@ -233,7 +233,8 @@ export async function bundleInstall(gemfile, lockFile, platform, engine, rubyVer
 }
 
 export async function addPlatform() {
-  const platform = (await exec.getExecOutput('ruby', ['-e', 'p Gem::Platform.local.to_s'])).stdout
+  const output = (await exec.getExecOutput('ruby', ['-e', 'p Gem::Platform.local.to_s'])).stdout
+  const platform = output.slice(1, output.length - 1)
   console.log(`Add Platform: ${platform}`)
   await exec.exec('bundle', ['lock', '--add-platform', platform])
   return true
--- dist/index.js
+++ dist/index.js
@@ -248,7 +248,8 @@ async function bundleInstall(gemfile, lockFile, platform, engine, rubyVersion, b
 }
 
 async function addPlatform() {
-  const platform = (await exec.getExecOutput('ruby', ['-e', 'p Gem::Platform.local.to_s'])).stdout
+  const output = (await exec.getExecOutput('ruby', ['-e', 'p Gem::Platform.local.to_s'])).stdout
+  const platform = output.slice(1, output.length - 1)
   console.log(`Add Platform: ${platform}`)
   await exec.exec('bundle', ['lock', '--add-platform', platform])
   return true
